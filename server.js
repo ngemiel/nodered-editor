e9832c8964a30d1fdaf87137f1244497d4ba9627
ngemiel <ringinbambu@gmail.com>
2025-11-20 23:12:00 +0700

    Refactor: Move static files to public folder and update server.js

--- server.js
+++ server.js
@@ -1,10 +1,5 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
-app.get('/', (req, res) => {
-  res.send(`
-    <h2>Editor Node-RED @ngemiel</h2>
-    <a href="/editor.html">Buka Editor →</a>
-  `);
-});
+
 app.listen(3000, () => console.log('Server jalan!'));
